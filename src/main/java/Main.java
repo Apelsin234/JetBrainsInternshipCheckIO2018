288f8079b9eb5e9222329ee9fddaeb69920936a7
Alexander <ratatuy.1995@mail.ru>
2018-05-06 01:07:47 +0300

    Print n the most popular courses.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import javafx.util.Pair;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
@@ -5,45 +6,73 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
 
+    private final static int EXPECTED_COUNT_OF_COURSES = 3000;
+
+    private static StepikApi stepikApi;
 
     public static void main(String[] args) {
-        StepikApi stepikApi;
+        int n = 10;
+        initRetrofit();
+        List<Pair<String, Integer>> courses = getListCourses();
+        printNFavoriteCourses(courses, n);
+    }
+
+    private static void printNFavoriteCourses(List<Pair<String, Integer>> courses, int n) {
+        courses.sort((a, b) -> 0 - Integer.compare(a.getValue(), b.getValue()));
+        for(int i = 0; i < Math.min(n, courses.size()); i++) {
+            System.out.println(i + 1 + ") " + courses.get(i).getKey() + " : " + courses.get(i).getValue());
+        }
+    }
+
+    private static List<Pair<String, Integer>> getListCourses() {
+        int numPage = 1;
+//        int count = 1;
+        List<Pair<String, Integer>> coursesAns = new ArrayList<>(EXPECTED_COUNT_OF_COURSES);
+        while (true) {
+            Response<JsonModel> response;
+            try {
+                response = stepikApi.getPage(numPage).execute();
+            } catch (IOException e) {
+                throw new RuntimeException("An error was occurred when get page.");
+            }
+            if (response != null && response.isSuccessful()) {
+                JsonModel json = response.body();
+                List<JsonModel.Course> courses = json.getCourses();
+
+                coursesAns.addAll(courses.stream()
+                        .map(course -> new Pair<>(course.getTitle(), course.getLearnersCount()))
+                        .collect(toList()));
+//                for (JsonModel.Course it : courses) {
+//                    System.out.println(count++ + ") " + it.getTitle() + " : " + it.getLearnersCount());
+//                }
+                numPage++;
+                if (!json.getMeta().hasNext()) {
+                    break;
+                }
+            } else {
+                throw new RuntimeException("An error occurred during networking ");
+            }
+
+        }
+        return coursesAns;
+
+    }
+
+    private static void initRetrofit() {
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://stepic.org")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         stepikApi = retrofit.create(StepikApi.class);
-        int i = 1;
-        int num = 1;
-        while (true) {
-            Response<JsonModel> response = null;
-            try {
-                response = stepikApi.getPage(i).execute();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            if (response != null && response.isSuccessful()) {
-                JsonModel json = response.body();
-                List<JsonModel.Course> courses = json.getCourses();
-                for (JsonModel.Course it : courses) {
-                    System.out.println(num++ + ") " + it.getTitle() + " : " + it.getLearnersCount());
-
-                }
-                i++;
-                if(!json.getMeta().hasNext()) {
-                    break;
-                }
-            } else {
-                throw new RuntimeException("An error was occurred when get response. ");
-            }
-
-        }
-
-
     }
 
 
